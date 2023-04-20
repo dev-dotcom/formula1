005632d0655c69e2eaceb0b37f4c8f2f597a9e79
dev-dotcom <devfadadu118@gamil.com>
2023-04-20 15:01:18 +0530

    docker files added

---
+++ Formula1/webapp.py
@@ -0,0 +1,6 @@
+from Formula1 import app
+from Formula1 import index
+from Formula1.auth import auth, jwt_auth
+from Formula1.Users import Users_2018, Users_2019, Users_2020, Users_2021, Users_2022,drivers
+from Formula1.Admin import Admin2018, Admin2019, Admin2020, Admin2021, Admin2022
+from Formula1.APIs import api2018, api2019, api2020, api2021 ,api2022
